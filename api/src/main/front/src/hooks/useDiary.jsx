69672b30a105c847aa3beb1db1a464437e618e4e
rizzy <12hyojinkim14@gmail.com>
2024-08-07 13:37:00 +0900

    feat: front-end 연동

---
+++ api/src/main/front/src/hooks/useDiary.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const useDiary = (id) => {
+    const [detailData, setDetailData] = useState(null);
+
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.get(`http://localhost:8080/api/diary/${id}`);
+            console.log(response);
+            setDetailData(response.data.body);
+          } catch (error) {
+            console.error(error);
+          }
+        };
+    
+        fetchData();
+      }, []);
+
+      return detailData
+}
+
+export default useDiary
